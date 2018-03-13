4c1cc6f17aff855e1aa569d7c0fa0cc9cd946798
Dormanator <ryandormanthe1@gmail.com>
2018-03-13 10:32:56 -0500

    proper routes for tags created

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    passport = require('passport'),
-    localStrategy = require('passport-local'),
-    methodOverride = require('method-override'),
-    bodyParser = require('body-parser'),
-    flash = require('connect-flash'),
-    Photo = require('./models/photo'),
-    User = require('./models/user'),
-    seedDB = require('./seeds');
+const express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  localStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  bodyParser = require("body-parser"),
+  flash = require("connect-flash"),
+  Photo = require("./models/photo"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
 // LOCAL ENV VARS
-require('dotenv').config();
+require("dotenv").config();
 
 // IMPORT IN ROUTES
-const photoRoutes = require('./routes/photos'),
-    userRoutes = require('./routes/users'),
-    contactRoutes = require('./routes/contact'),
-    indexRoutes = require('./routes/index');
+const photoRoutes = require("./routes/photos"),
+  tagRoutes = require("./routes/tags");
+(userRoutes = require("./routes/users")),
+  (contactRoutes = require("./routes/contact")),
+  (indexRoutes = require("./routes/index"));
 
 // APP CONFIG
 mongoose.connect(process.env.DATABASEURL);
@@ -26,18 +27,20 @@ mongoose.connect(process.env.DATABASEURL);
 //seedDB();
 
 // SERVER CONFIG
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash());
 
 // PASSPORT CONFIG
-app.use(require('express-session')({
+app.use(
+  require("express-session")({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()));
@@ -46,19 +49,20 @@ passport.deserializeUser(User.deserializeUser());
 
 // GLOBAL MIDDLEWARE TO CREATE VARS ACCESSIBLE IN VIEWS
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash('error');
-    res.locals.success = req.flash('success');
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 // USING ROUTER
-app.use('/photos', photoRoutes);
-app.use('/contact', contactRoutes);
-app.use('/', userRoutes);
-app.use('/', indexRoutes);
+app.use("/photos", photoRoutes);
+app.use("/tags", tagRoutes);
+app.use("/contact", contactRoutes);
+app.use("/", userRoutes);
+app.use("/", indexRoutes);
 
 // SERVER LISTEN SETUP
 app.listen(process.env.PORT, process.env.IP, () => {
-    console.log('Server Listening on 3000');
+  console.log("Server Listening on 3000");
 });
--- routes/photos.js
+++ routes/photos.js
@@ -1,130 +1,148 @@
-const express = require('express'),
-    router = express.Router(),
-    multer = require('multer'),
-    Photo = require('../models/photo'),
-    middleware = require('../middleware/index');
+const express = require("express"),
+  router = express.Router(),
+  multer = require("multer"),
+  Photo = require("../models/photo"),
+  middleware = require("../middleware/index");
 
 // MULTER SETUP FOR HANDLING IMAGE FILE
 const storage = multer.diskStorage({
     filename: (req, file, cb) => {
-        cb(null, Date.now() + file.originalname);
+      cb(null, Date.now() + file.originalname);
     }
-}),
-    imageFilter = (req, file, cb) => {
-        const MAX_FILE_SIZE_BYTES = 3000000;
-        // accept only images
-        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-            return cb(new Error('Only image files are allowed!'), false);
-            // that are no greater than 3mb
-        } else if (file.size > MAX_FILE_SIZE_BYTES) {
-            return cb(new Error('Image must be 3MB or less'), false);
-        }
-        cb(null, true);
-    };
+  }),
+  imageFilter = (req, file, cb) => {
+    const MAX_FILE_SIZE_BYTES = 3000000;
+    // accept only images
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+      return cb(new Error("Only image files are allowed!"), false);
+      // that are no greater than 3mb
+    } else if (file.size > MAX_FILE_SIZE_BYTES) {
+      return cb(new Error("Image must be 3MB or less"), false);
+    }
+    cb(null, true);
+  };
 const upload = multer({ storage: storage, fileFilter: imageFilter });
 
 // INDEX - List all entries
-router.get('/', (req, res) => {
-    Photo.find({}, (err, foundPhotos) => {
-        if (err) {
-            req.flash('error', 'Opps, something went wrong with the database query. Please try again.');
-            res.redirect('back');
-        } else {
-            res.render('photos/index', { photos: foundPhotos });
-        }
-    });
+router.get("/", (req, res) => {
+  Photo.find({}, (err, foundPhotos) => {
+    if (err) {
+      req.flash(
+        "error",
+        "Opps, something went wrong with the database query. Please try again."
+      );
+      res.redirect("back");
+    } else {
+      res.render("photos/index", { photos: foundPhotos });
+    }
+  });
 });
 
 // NEW - New photo form
-router.get('/new', middleware.isLoggedIn, (req, res) => {
-    res.render('photos/new');
+router.get("/new", middleware.isLoggedIn, (req, res) => {
+  res.render("photos/new");
 });
 
 // CREATE - Add a new photo, then redirect
-router.post('/', middleware.isLoggedIn, upload.single('image'), middleware.uploadImg, (req, res) => {
+router.post(
+  "/",
+  middleware.isLoggedIn,
+  upload.single("image"),
+  middleware.uploadImg,
+  (req, res) => {
     req.body.photo.image = req.image;
     req.body.photo.author = {
-        id: req.user._id,
-        username: req.user.username
+      id: req.user._id,
+      username: req.user.username
     };
     Photo.create(req.body.photo, (err, photo) => {
-        if (err) {
-            req.flash('error', 'Opps, something went wrong saving your photo. Please try again.');
-            return res.redirect('back');
-        }
-        req.flash('success', 'Your photo has been added!');
-        res.redirect('/photos/' + photo.id);
+      if (err) {
+        req.flash(
+          "error",
+          "Opps, something went wrong saving your photo. Please try again."
+        );
+        return res.redirect("back");
+      }
+      req.flash("success", "Your photo has been added!");
+      res.redirect("/photos/" + photo.id);
     });
-});
-
-// filter - show select group of photos based on value
-router.get('/tag/:category', (req, res) => {
-    const tags = ['people', 'places', 'things', 'animals'];
-    if (tags.includes(req.params.category)) {
-        Photo.find({ 'tag': req.params.category }, (err, foundPhotos) => {
-            if (err) {
-                req.flash('error', 'Opps, something went wrong with the database query. Please try again.');
-                res.redirect('back');
-            } else {
-                res.render('photos/index', { photos: foundPhotos });
-            }
-        });
-    } else {
-        req.flash('error', 'Not a valid photo tag');
-        res.redirect('/photos');
-    }
-});
+  }
+);
 
 // SHOW - Show info about a specific photo
-router.get('/:id', (req, res) => {
-    Photo.findById(req.params.id, (err, foundPhoto) => {
-        if (err || !foundPhoto) {
-            req.flash('error', 'Opps, something went wrong retrieving the photo. Please try again.');
-            res.redirect('back');
-            console.log(err);
-        } else {
-            res.render('photos/show', { photo: foundPhoto });
-        }
-    });
+router.get("/:id", (req, res) => {
+  Photo.findById(req.params.id, (err, foundPhoto) => {
+    if (err || !foundPhoto) {
+      req.flash(
+        "error",
+        "Opps, something went wrong retrieving the photo. Please try again."
+      );
+      res.redirect("back");
+      console.log(err);
+    } else {
+      res.render("photos/show", { photo: foundPhoto });
+    }
+  });
 });
 
 // EDIT - Show edit form for one entry
-router.get('/:id/edit', middleware.checkPhotoOwnership, (req, res) => {
-    res.render('photos/edit', { photo: req.photo });
+router.get("/:id/edit", middleware.checkPhotoOwnership, (req, res) => {
+  res.render("photos/edit", { photo: req.photo });
 });
 
 // UPDATE - Update a single entry, then redirect somewhere
-router.put('/:id', middleware.checkPhotoOwnership, upload.single('image'), middleware.uploadImg, (req, res) => {
+router.put(
+  "/:id",
+  middleware.checkPhotoOwnership,
+  upload.single("image"),
+  middleware.uploadImg,
+  (req, res) => {
     // if a new file was given save the new image object with an updated url
     // the version number in teh url will change and prevent caching problems
     if (Boolean(req.file)) {
-        req.body.photo.image = req.image;
+      req.body.photo.image = req.image;
     }
     // else just update the db entry
-    Photo.findByIdAndUpdate(req.params.id, req.body.photo, (err, updatedPhoto) => {
+    Photo.findByIdAndUpdate(
+      req.params.id,
+      req.body.photo,
+      (err, updatedPhoto) => {
         if (err) {
-            req.flash('error', 'Opps, something went wrong saving your updates. Please try again.');
-            res.redirect('/photos');
-            console.log(err);
+          req.flash(
+            "error",
+            "Opps, something went wrong saving your updates. Please try again."
+          );
+          res.redirect("/photos");
+          console.log(err);
         } else {
-            req.flash('success', 'You\'ve updated your photo!');
-            res.redirect('/photos/' + req.params.id);
+          req.flash("success", "You've updated your photo!");
+          res.redirect("/photos/" + req.params.id);
         }
-    });
-});
+      }
+    );
+  }
+);
 
 // DESTROY - Delete a single entry, then redirect somewhere
-router.delete('/:id', middleware.checkPhotoOwnership, middleware.deleteImg, (req, res) => {
-    Photo.findByIdAndRemove(req.params.id, (err) => {
-        if (err) {
-            req.flash('error', 'Opps, something went wrong deleting your photo. Please try again.');
-            res.redirect('back');
-            console.log(err);
-        } else {
-            req.flash('success', 'Your photo was deleted!');
-            res.redirect('/photos');
-        }
+router.delete(
+  "/:id",
+  middleware.checkPhotoOwnership,
+  middleware.deleteImg,
+  (req, res) => {
+    Photo.findByIdAndRemove(req.params.id, err => {
+      if (err) {
+        req.flash(
+          "error",
+          "Opps, something went wrong deleting your photo. Please try again."
+        );
+        res.redirect("back");
+        console.log(err);
+      } else {
+        req.flash("success", "Your photo was deleted!");
+        res.redirect("/photos");
+      }
     });
-});
+  }
+);
 
 module.exports = router;
