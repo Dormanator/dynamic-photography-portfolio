91e61f7c1ef7bf2e13b0d6663a3365563c834bb4
Dormanator <ryandormanthe1@gmail.com>
2018-03-14 11:01:35 -0500

    formatting of files adjusted

--- models/photo.js
+++ models/photo.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // MONGOOSE/MODEL CONFIG
 const photoSchema = new mongoose.Schema({
-    title: String,
-    image: {
-        id: String,
-        thumb: String,
-        small: String,
-        medium: String,
-        large: String
+  title: String,
+  image: {
+    id: String,
+    thumb: String,
+    small: String,
+    medium: String,
+    large: String
+  },
+  alt: String,
+  tag: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     },
-    alt: String,
-    tag: String,
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        username: String
-    },
-    date: { type: Date, default: Date.now }
+    username: String
+  },
+  date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Photo', photoSchema);
+module.exports = mongoose.model("Photo", photoSchema);
